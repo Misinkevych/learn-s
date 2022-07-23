6596c9b47648cb7f62c695b9154d36b0e093b61d
Alina Misinkrvych <misinkevych.alina@gmail.com>
2022-07-23 17:05:19 +0300

    validator

---
+++ src/app.js
@@ -0,0 +1,74 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal } from 'bootstrap';
+import Api from './api'
+
+console.log('im here')
+
+function init() {
+
+    var myModal = new Modal(document.getElementById('exampleModal'))
+
+    renderTodos()
+
+    document.getElementById('onSaveTodo').addEventListener('click', () => {
+        const input = document.getElementById('toDoName')
+        Api.addTodo({
+            name: input.value,
+            id: new Date().getTime()
+        });
+        myModal.hide()
+        input.value = ''
+        renderTodos()
+    })
+
+    document.getElementById('container').addEventListener('click', (e) => {
+        const el = e.target
+        if (el.classList.contains('remove-todo')) {
+            const liEl = el.closest('li')
+            // const allLi = Array.from(document.querySelectorAll('#container li'))
+            // const numberToRemove = allLi.indexOf(liEl)
+            // Api.removeTodo(numberToRemove);
+
+            Api.removeById(liEl.id)
+            renderTodos()
+        }
+    }, {capture: true})
+
+}
+
+function renderTodos() {
+
+    document.getElementById('container').innerHTML = ''
+
+    const data = Api.getData()
+
+    data.forEach((item) => {
+        const li = document.createElement('li')
+        li.id = item.id
+        li.innerHTML =
+            `<div class="input-group mb-3">
+                <div class="input-group-text">
+                    <input class="form-check-input mt-0" type="checkbox" value="">
+                </div>
+                <input type="text" value="${item.name}" class="form-control">
+                <div class="input-group-text">
+                    <button type="button" class="btn btn-primary remove-todo">Remove</button>
+                </div>
+            </div>`;
+        document.getElementById('container').appendChild(li)
+
+    })
+    document.getElementById('container').onblur = function() {
+        console.log(this.value.length);
+        if (this.getAttribute('data-length') > this.value.length) {
+          this.classList.remove('valid');
+          this.classList.add('invalid');
+        } else {
+          this.classList.remove('invalid');
+          this.classList.add('valid');
+        }
+      };
+
+}
+
+init()
